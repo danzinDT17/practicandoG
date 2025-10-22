a3e36ac5b8531a59588dbfb1defc688f21d86e6c
danzinDT17 <tarazonadaniel49@gmail.com>
2025-10-22 14:53:23 -0500

    Proyecto subiendo

--- src/main/java/com/mycompany/proyectogit/ProyectoGit.java
+++ src/main/java/com/mycompany/proyectogit/ProyectoGit.java
@@ -1,14 +1,19 @@
 
 package com.mycompany.proyectogit;
+import java.util.Scanner;
 
 public class ProyectoGit {
 
     public static void main(String[] args) {
+        Scanner ingreso = new Scanner (System.in);        
+        int a,b;
         
-        int a=20;
-        int b=25;
-        
+        System.out.println("Ingresa el valor de a:");
+        a = ingreso.nextInt();
+        System.out.println("Ingresa el valor de b:");
+        b = ingreso.nextInt();
         System.out.println("La suma es:"+(a+b));
+        
         
     }
 }
