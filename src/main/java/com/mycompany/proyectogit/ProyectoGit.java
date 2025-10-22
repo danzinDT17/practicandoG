e0ea454bda3fdcbd0cf67492fd5cf16bf3c13fbd
danzinDT17 <tarazonadaniel49@gmail.com>
2025-10-22 14:10:21 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/proyectogit/ProyectoGit.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.proyectogit;
+
+public class ProyectoGit {
+
+    public static void main(String[] args) {
+        
+        int a=20;
+        int b=25;
+        
+        System.out.println("La suma es:"+(a+b));
+        
+    }
+}
